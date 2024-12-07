98979866eb5c0e80a9d31c9fd0057963ee945ccc
HrithikSampson <hrithikedwardsampson@gmail.com>
2024-12-08 04:36:25 +0530

    feat: risc-zero build

--- negotiate_project/host/src/main.rs
+++ negotiate_project/host/src/main.rs
@@ -4,65 +4,82 @@ use methods::{
     NEGOTIATE_GUEST_ELF, NEGOTIATE_GUEST_ID
 };
 use risc0_zkvm::{default_prover, ExecutorEnv};
+use serde::{Serialize,Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 struct AuctionBids{
     min_price: u64,
     max_price: u64,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct AuctionArena{
     bids: Vec<AuctionBids>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Auctioneer {
+    pub num_players: u8,
+    pub strategy: Strategy,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Strategy {
+    Auction,
+    Negotiate,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Inputs{
+    public_input: Auctioneer,
+    public_output: AuctionArena,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Outputs{
+    public_input: Auctioneer,
+    public_output: u64,
+}
+
 fn main() {
-    let client = Client::from_env(risc0_zkvm::VERSION)?;
+    // let client = Client::from_env(risc0_zkvm::VERSION)?;
     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
 
-    let result: Type = write_slice(&output)?;
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
-        .init();
+    let auctioneeer_input: Auctioneer = Auctioneer {
+        num_players: 2,
+        strategy: Strategy::Negotiate,
+    };
+    let auction_input: AuctionArena = AuctionArena {
+        bids: vec![AuctionBids{
+            min_price: 30,
+            max_price: 70,
+        },
+        AuctionBids{
+            min_price: 50,
+            max_price: 100,
+        }]
+    };
 
-    // An executor environment describes the configurations for the zkVM
-    // including program inputs.
-    // A default ExecutorEnv can be created like so:
-    // `let env = ExecutorEnv::builder().build().unwrap();`
-    // However, this `env` does not have any inputs.
-    //
-    // To add guest input to the executor environment, use
-    // ExecutorEnvBuilder::write().
-    // To access this method, you'll need to use ExecutorEnv::builder(), which
-    // creates an ExecutorEnvBuilder. When you're done adding input, call
-    // ExecutorEnvBuilder::build().
+    let auction_val: Inputs = Inputs {
+        public_input: auctioneeer_input,
+        public_output: auction_input,
+    };
 
-    // For example:
-    let input: u32 = 15 * u32::pow(2, 27) + 1;
-    let env = ExecutorEnv::builder()
-        .write(&input)
-        .unwrap()
-        .build()
-        .unwrap();
+    let env = ExecutorEnv::builder().write(&auction_val).unwrap().build().unwrap();
 
-    // Obtain the default prover.
+
+
     let prover = default_prover();
+    let receipt = prover.prove(env, NEGOTIATE_GUEST_ELF).unwrap().receipt;
 
-    // Proof information by proving the specified ELF binary.
-    // This struct contains the receipt along with statistics about execution of the guest
-    let prove_info = prover
-        .prove(env, NEGOTIATE_GUEST_ELF)
-        .unwrap();
+    let public_data: Outputs = receipt.journal.decode().unwrap();
 
-    // extract the receipt.
-    let receipt = prove_info.receipt;
-
-    // TODO: Implement code for retrieving receipt journal here.
-
-    // For example:
-    let _output: u32 = receipt.journal.decode().unwrap();
+    println!("{:?}", public_data);
 
     // The receipt was verified at the end of proving, but the below code is an
     // example of how someone else could verify this receipt.
     receipt
         .verify(NEGOTIATE_GUEST_ID)
         .unwrap();
+
+    // Ok(());
 }
--- negotiate_project/methods/guest/src/main.rs
+++ negotiate_project/methods/guest/src/main.rs
@@ -1,13 +1,94 @@
 use risc0_zkvm::guest::env;
+use serde::{Serialize,Deserialize};
 
-fn main() {
-    // TODO: Implement your guest code here
+use negotiate as negotiation;
 
-    // read the input
-    let input: u32 = env::read();
+#[derive(Serialize, Deserialize, Debug)]
+struct AuctionBids {
+    pub min_price: u64,
+    pub max_price: u64,
+}
 
-    // TODO: do something with the input
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+struct Auctioneer {
+    pub num_players: u8,
+    pub strategy: Strategy,
+}
 
-    // write public output to the journal
-    env::commit(&input);
+// adapters
+impl From<Auctioneer> for negotiation::Auctioneer {
+    fn from(auctioneer: Auctioneer) -> Self {
+        negotiation::Auctioneer {
+            num_players: auctioneer.num_players,
+        }
+    }
+}
+
+impl From<AuctionBids> for negotiation::AuctionBids {
+    fn from(bid: AuctionBids) -> Self {
+        negotiation::AuctionBids {
+            min_price: bid.min_price,
+            max_price: bid.max_price,
+        }
+    }
+}
+
+impl From<AuctionArena> for negotiation::AuctionArena {
+    fn from(arena: AuctionArena) -> Self {
+        negotiation::AuctionArena {
+            bids: arena.bids.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+enum Strategy {
+    Auction,
+    Negotiate,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AuctionArena {
+    bids: Vec<AuctionBids>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Inputs{
+    private_input: AuctionArena,
+    public_input: Auctioneer,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Outputs{
+    public_input: Auctioneer,
+    public_output: u64,
+}
+
+pub fn main() {
+    // Read an input to the program.
+    //
+    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
+    // from the prover.
+    let auction_inputs: Inputs = env::read::<Inputs>();
+    let auctioneer = auction_inputs.public_input;
+    let auction_arena = auction_inputs.private_input;
+    env::commit(&auctioneer);
+
+
+    let strategy_function = negotiation::negotiate_strategy; // Adjusted the naming convention
+
+
+    let auctioneer_clone = auctioneer.clone();
+    let amount = strategy_function(
+        negotiation::Auctioneer::from(auctioneer),
+        negotiation::AuctionArena::from(auction_arena),
+    );
+
+    let output: Outputs = Outputs {
+        public_input: auctioneer_clone,
+        public_output: amount,
+    };
+    // Encode the public values of the program.
+    env::commit::<Outputs>(&output);
 }
